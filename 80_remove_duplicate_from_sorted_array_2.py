91014d67e28023528bf842d26337cd68d31bbcf0
tabletenniser <tabletenniser@gmail.com>
2020-01-16 23:55:01 -0800

    Here's a O(n^2) solution

--- 80_remove_duplicate_from_sorted_array_2.py
+++ 80_remove_duplicate_from_sorted_array_2.py
@@ -16,7 +16,28 @@ def removeDuplicates(nums):
         j += 1
     return i
 
-# n = [1,1,1,2,2,3]
-n = [0,0,1,1,1,1,2,3,3]
-print removeDuplicates(n)
+def removeDuplicates2(nums):
+    i, j, c, r = 0, 0, 0, 0
+    while j < len(nums):
+        # print i,j,c,r,nums
+        if nums[i] == nums[j]:
+            c += 1
+            j += 1
+            continue
+        assert c > 0
+        if c == 1:
+            i = j
+            r += 1
+        else:
+            i += 2
+            nums[i:] = nums[j:]
+            j = i
+            r += 2
+        c = 0
+    r += min(c, 2)
+    return r
+
+n = [1,1,1]
+# n = [0,0,1,1,1,1,2,3,3]
+print removeDuplicates2(n)
 print n
